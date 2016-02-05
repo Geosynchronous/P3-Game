d6926337981e04affbba2a5be2556cf36df5b15b
George Fischer <geosynchronous@mac.com>
2016-02-04 17:25:44 -0700

    Enemies have various velocities
    Employed a PseudoClassical Class form to the enemy fn and passed
    velocity parameters to various enemy instances.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // Enemies our player must avoid
-var enemy = function() {
+// PseudoClassical Class Definition Function used here
+// velocity sets relative default speed of enemy as global variable
+var velocity = 1;
+var enemy = function(velocity) {
+    this.velocity = velocity;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -7,9 +11,8 @@ var enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
-    //TODO - Added by Geo to establish starting location
-    // Dependencies to consider.  Row starts on, grid start reference point.
-    // enemy-bug.png is 101 x 171, note the enemy bug is actually smaller due to alpha background
+    // Establish position starting point
+    // enemy-bug.png is 101 x 171, enemy visual is actually smaller due to alpha background
     // x = 0, y = 63 perfectly centers enemy on first ronw first square tile
     // x = -95 offsets most bug off canvas with only a little nose showing
     // this is a good place to start for the first enemy on first brick row
@@ -26,25 +29,16 @@ enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-    // TODO - finish this
-    // The enemy will need to be centered between the rows and is a constant dy over dt
-    // The enemy will need to move dy as a function of dt accross the columns
+    // Sets Boundaries for start and end of Enemy travel.
+    // Incremenmts movement of enemy within boundaries.
+    // Increment relative velocity set for each enemy as passed parameter.
+    // dt sets relatively stable time increment value on all computers.
+    //
     if (this.x > 600) {
         this.x = -95;
     } else {
-        this.x = this.x + (300 * dt);
+        this.x = this.x + (300 * this.velocity * dt);
     }
-
-
-    // console.log(dt);
-    // console.log(this.x);
-    // console.log(this);
-    // console.log(enemy);
-    // console.log(allEnemies);
-    // console.log(allEnemies[0]);
-    console.log(allEnemies[1]);
-    //this.y = this.y * (1 + dt);
-
 };
 
 // Draw the enemy on the screen, required method for game
@@ -59,13 +53,14 @@ enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-//TODO - Temporary, to avoid errors.  Need to finish...
+// Individual velocity parameters passed to various enemies.
+// Row offset passed into y values for row centering
+// allEnemies is global array, and allEnemy[] elements have enemy prototype
 var allEnemies = [];
-allEnemies[0] = new enemy;
-allEnemies[1] = new enemy;
+allEnemies[0] = new enemy(1);
+allEnemies[1] = new enemy(1.3);
 allEnemies[1].y = 147;
-// allEnemies[1].x = this.x;
-allEnemies[2] = new enemy;
+allEnemies[2] = new enemy(2.1);
 allEnemies[2].y = 233;
 
 
--- js/engine.js
+++ js/engine.js
@@ -42,9 +42,6 @@ var Engine = (function(global) {
          */
         var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
-            // TODO - Use as needed, Remove when done
-            // console.log(now);
-            // console.log(dt);
 
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
