2b231f4437a069d85d9bf9917790afc502d6d1f5
George Fischer <geosynchronous@mac.com>
2016-02-13 18:02:55 -0700

    Info Window Text
    Added text to the INFO WINDOW via JS by a brute force method.  The text
    are the basic instructions for the game.

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -19,16 +19,42 @@
     }
 
     // Displays Info Window
+    // Canvas is not well equipe for paragraphs and line breaks!!!
+    // Brute Force multiline fillText used here
+    // TODO--- HTML & CSS or a JS Library are the way to go?
     var InfoWindow = function() {
         var my_gradientBoard = ctx.createLinearGradient(0, 0, 0, 300);
         my_gradientBoard.addColorStop(0, "transparent");
         my_gradientBoard.addColorStop(1, "rgba(0, 0, 0, 0.7");
         ctx.fillStyle = my_gradientBoard;
         ctx.fillRect(0, 50, 505, 460);
+
+
         ctx.fillStyle = '#fbcc09';
         ctx.textBaseline = 'top';
         ctx.font = 'bold 24px sans-serif';
         ctx.fillText('INFO', 20, 70);
+        ctx.font = 'bold 18px sans-serif';
+
+        ctx.fillText('When the PLAY button is clicked, a PLAYER appears', 20, 110);
+        ctx.fillText('on the grass.  Pressing the arrow keys on keyboard', 20, 130);
+        ctx.fillText('moves the PLAYER a step in the direction of the key.', 20, 150);
+
+        ctx.fillText('A point is scored if the PLAYER can get to the water.', 20, 190);
+        ctx.fillText('If a PLAYER hits a BUG, the GAME IS OVER and all', 20, 210);
+        ctx.fillText('the points won will be lost.', 20, 230);
+
+        ctx.fillText('When PLAYER scores 10 points they win the GAME!', 20, 270);
+        ctx.fillText('Winning a GAME starts the next LEVEL. It is then', 20, 290);
+        ctx.fillText('up to the PLAYER to figure things out.', 20, 310);
+
+        ctx.fillText('There are five increasingly difficult LEVELS to win', 20, 350);
+        ctx.fillText('to become a JEDI JITTERBUGGING MASTER.', 20, 370);
+
+        ctx.fillStyle = '#cc0000';
+        ctx.fillText('WARNING: These bugs are experiencing an epidemic', 20, 410);
+        ctx.fillText('of a once thought to be extinct JITTER VIRUS that is', 20, 430);
+        ctx.fillText('rapidly evolving mysterious strange behaviours.', 20, 450);
     }
 
     // Play Button allows user to start playing game when selected
