7a75af6d75b5ee3acf9d101b0c0f17454ea0b886
George Fischer <geosynchronous@mac.com>
2016-02-25 15:25:25 -0700

    6 to 3 digit Hex colors
    Was not aware that I could convert all forms of 6 digit to three digit
    and only have slight shade variance.  Worked fine for me.

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -17,15 +17,15 @@
     // Displays Scoreboard
     var ScoreBoardRender = function() {
         var my_gradientBoard = ctx.createLinearGradient(0, 0, 0, 50);
-        my_gradientBoard.addColorStop(0, "#666666");
-        my_gradientBoard.addColorStop(1, "#000");
+        my_gradientBoard.addColorStop(0, '#666');
+        my_gradientBoard.addColorStop(1, '#000');
         ctx.fillStyle = my_gradientBoard;
         ctx.fillRect(0, 0, 505, 47);
     };
 
     // Displays LifeCycle label and value (on Scoreboard)
     var LifeCycleRender = function(lifeCycle) {
-        ctx.fillStyle = '#fbcc09';
+        ctx.fillStyle = '#fc1';
         ctx.textBaseline = 'top';
         ctx.font = 'bold 24px sans-serif';
         ctx.fillText('LIFECYCLE', 170, 10);
@@ -34,7 +34,7 @@
 
     // Displays Score label and value (on Scoreboard)
     var ScoreRender = function(playerScore) {
-        ctx.fillStyle = '#fbcc09';
+        ctx.fillStyle = '#fc1';
         ctx.textBaseline = 'top';
         ctx.font = 'bold 24px sans-serif';
         ctx.fillText('Score: ', 10, 10);
@@ -63,7 +63,7 @@
             ctx.fillText('J I T T E R   B U G S', 101 + Math.sin(Date.now()), 529 + Math.cos(Date.now()));
 
             // Title Jitters with Trig fn
-            ctx.fillStyle  = '#fbcc09';
+            ctx.fillStyle  = '#fc1';
             ctx.textBaseline = 'top';
             ctx.font = 'bold 32px sans-serif';
             ctx.fillText('J I T T E R   B U G S', 101 + Math.cos(Date.now()), 530);
@@ -94,9 +94,9 @@
     // Generic prototype function to render button
     MakeButton.prototype.render = function() {
         var my_gradient = ctx.createLinearGradient(0, 0, 0, 70);
-        my_gradient.addColorStop(0, "#cc0000");
-        my_gradient.addColorStop(1, "#000");
-        ctx.fillStyle  = '#936c6c';
+        my_gradient.addColorStop(0, '#c00');
+        my_gradient.addColorStop(1, '#000');
+        ctx.fillStyle  = '#966';
         ctx.fillRect(this.xBegin + 2, this.yBegin + 1, this.xEnd - 1, this.yEnd);
         ctx.fillStyle = my_gradient;
         ctx.fillRect(this.xBegin, this.yBegin, this.xEnd, this.yEnd);
@@ -135,7 +135,7 @@
     // Brute Force multiline fillText used here
     // TODO--- HTML & CSS or a JS Library are the way to go?
     var InfoRender = function() {
-            ctx.fillStyle = '#fbcc09';
+            ctx.fillStyle = '#fc1';
             ctx.textBaseline = 'top';
             ctx.font = 'bold 24px sans-serif';
             ctx.fillText('INFO', 20, 70);
@@ -156,7 +156,7 @@
             ctx.fillText('There are five increasingly difficult LIFECYCLES to', 20, 350);
             ctx.fillText('WIN to become a JEDI JITTERBUGGING MASTER.', 20, 370);
 
-            ctx.fillStyle = '#cc0000';
+            ctx.fillStyle = '#c00';
             ctx.fillText('WARNING: These bugs are experiencing an epidemic', 20, 410);
             ctx.fillText('of a once thought to be extinct JITTER VIRUS that is', 20, 430);
             ctx.fillText('rapidly evolving strange behavioural phenotypes.', 20, 450);
@@ -166,7 +166,7 @@
 
     // Displays Jitter Animated GAME OVER in a Message Window
     var GameOverRender = function() {
-        ctx.fillStyle = '#fbcc09';
+        ctx.fillStyle = '#fc1';
         ctx.textBaseline = 'top';
         ctx.font = 'bold 72px sans-serif';
 
@@ -177,7 +177,7 @@
     };
 
     var WonGameRender = function() {
-        ctx.fillStyle = '#fbcc09';
+        ctx.fillStyle = '#fc1';
         ctx.textBaseline = 'top';
         ctx.font = 'bold 72px sans-serif';
 
@@ -188,7 +188,7 @@
     };
 
     var NinjaRender = function() {
-        ctx.fillStyle = '#fbcc09';
+        ctx.fillStyle = '#fc1';
         ctx.textBaseline = 'top';
         ctx.font = 'bold 72px sans-serif';
 
@@ -199,7 +199,7 @@
 
     // Displays Heart Capture info in a Message Window
     var HeartCaptureRender = function() {
-        ctx.fillStyle = '#fbcc09';
+        ctx.fillStyle = '#fc1';
         ctx.textBaseline = 'top';
         ctx.font = 'bold 24px sans-serif';
         ctx.fillText('CONGRATULATIONS!!!', 20, 70);
